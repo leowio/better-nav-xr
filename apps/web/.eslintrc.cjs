421be3a4638545ea23681fc1804e70cc4b3a0f6f
Leo Wang <meimprankster@gmail.com>
2025-11-02 22:42:52 +0800

    remove unnecessary setup

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -1,5 +1,19 @@
-/** @type {import("eslint").Linter.Config} */
 module.exports = {
-  root: true,
-  extends: ["@repo/eslint-config/index.js"],
+  env: {
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+  },
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": "off",
+  },
 };
